14326bdbcf77aeeb025a0ccb61c058a40d938f13
Radouane Bahi <radbahi@gmail.com>
2019-12-03 22:06:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -54,3 +54,14 @@ i += 1
 end
   return true
 end
+
+def reduce_to_any_true(source_array)
+  def reduce_to_all_true(source_array)
+    i = 0 
+  while i < source_array.length do 
+    if source_array[i]
+      return true
+end
+i += 1 
+end
+end
